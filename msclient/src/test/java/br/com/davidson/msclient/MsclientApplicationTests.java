63fa5f83fe2bfb3f437999ac428f6cb356e28c1b
Davidson <dehpacifico@gmail.com>
2026-02-11 22:02:21 -0300

    estrutura inicial do estudo de microserviço com eureka server.

---
+++ msclient/src/test/java/br/com/davidson/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davidson.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
