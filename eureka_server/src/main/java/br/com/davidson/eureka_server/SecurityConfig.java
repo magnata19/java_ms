39592d48b1376405a8d068ed582ee57722ca420b
Davidson <dehpacifico@gmail.com>
2026-02-27 18:47:38 -0300

    criando imagem multi stage do serviço eureka

---
+++ eureka_server/src/main/java/br/com/davidson/eureka_server/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.davidson.eureka_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
